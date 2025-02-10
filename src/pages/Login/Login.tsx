ffd6f4eb2647623af1551754017aedb8995acc68
Fábio Papais <flfp@cin.ufpe.br>
2025-02-10 10:55:02 -0300

    fix: login response (token token)

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -82,7 +82,7 @@ function Login() {
     try {
       if (isLogin) {
         const data = await loginService({ email: formData.email, password: formData.password });
-        login(data.token.user, data.token.token);
+        login(data.user, data.token);
         toast({
           variant: "default",
           title: "Login Bem-Sucedido",
