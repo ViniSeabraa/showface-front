cb20d289165a13ce981b0bff33efa6590e8ca3df
Fábio Papais <flfp@cin.ufpe.br>
2025-03-31 19:42:38 -0300

    fix: hotfix creating neww event

--- src/pages/NewEvent/newEvent.tsx
+++ src/pages/NewEvent/newEvent.tsx
@@ -1,20 +1,24 @@
-import './newEvent.css';
-import Upload from '../../assets/UploadPhotos.svg'
-import { useState } from 'react';
+import "./newEvent.css";
+import Upload from "../../assets/UploadPhotos.svg";
+import { useState } from "react";
 import { useToast } from "../../components/hooks/use-toast";
-import { createEventService } from '../../services/eventService';
-import { AxiosError } from 'axios';
-import { useAuth } from '../../utils/authContext';
+import { createEventService } from "../../services/eventService";
+import { AxiosError } from "axios";
+import { useAuth } from "../../utils/authContext";
 
 function NewEvent() {
   const { getUserData } = useAuth();
-  const user = getUserData() || { name: "Usuário Desconhecido", email: "", id:""};
+  const user = getUserData() || {
+    name: "Usuário Desconhecido",
+    email: "",
+    id: "",
+  };
   const [formData, setFormData] = useState({
-    name: '',
-    photographer: '',
-    photographerLink: '',
-    userId: user.id || '',
-    userName: user.name || 'Usuário Desconhecido',
+    name: "",
+    photographer: "",
+    photographerLink: "",
+    userId: user.id || "",
+    userName: user.name || "Usuário Desconhecido",
     file: null as File | null,
   });
 
@@ -23,8 +27,6 @@ function NewEvent() {
     photographer: false,
     photographerLink: false,
   });
-
-
 
   const { toast } = useToast();
 
@@ -58,7 +60,7 @@ function NewEvent() {
 
     if (validateFields()) {
       toast({
-        variant: "destructive", 
+        variant: "destructive",
         title: "Erro de Validação",
         description: "Preencha todos os campos.",
       });
@@ -68,9 +70,9 @@ function NewEvent() {
     try {
       await createEventService({
         name: formData.name,
-        photographer: formData.photographer, 
+        photographer: formData.photographer,
         photographerLink: formData.photographerLink,
-        userId: formData.userId, 
+        userId: formData.userId,
         userName: formData.userName,
         file: formData.file,
       });
@@ -84,94 +86,104 @@ function NewEvent() {
       window.location.href = "/myEvents";
     } catch (error) {
       if (error instanceof AxiosError && error.response) {
-          if (error.response.status === 400) {
-            toast({
-              variant: "destructive",
-              title: "Evento com este nome já existe",
-              description: "Tente novamente.",
-            });
-          }
-          if (error.response.status == 500) {
-            toast({
-              variant: "destructive",
-              title: "Ocorreu um erro com o servidor",
-              description: "Aguarde e tente novamente mais tarde.",
-            });
-          }
+        if (error.response.status === 400) {
+          toast({
+            variant: "destructive",
+            title: "Evento com este nome já existe",
+            description: "Tente novamente.",
+          });
+        }
+        if (error.response.status == 500) {
+          toast({
+            variant: "destructive",
+            title: "Ocorreu um erro com o servidor",
+            description: "Aguarde e tente novamente mais tarde.",
+          });
+        }
       }
     }
-
   };
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
-      setFormData({ ...formData, file: event.target.files?.[0] })
+      setFormData({ ...formData, file: event.target.files?.[0] });
       toast({
         title: "Arquivo Selecionado",
-        description: `Arquivo: ${event.target.files?.[0]?.name || ''}`,
+        description: `Arquivo: ${event.target.files?.[0]?.name || ""}`,
       });
     }
   };
 
   return (
-    <div className='all'>
-        <h1 className='title'>Criar evento</h1>    
+    <div className="all">
+      <h1 className="title">Criar evento</h1>
 
-        <div className='container-father'>
-          <form noValidate onSubmit={handleSubmit}>
-            <label htmlFor="name">Nome do evento:</label>
-            <input
-              type="text"
-              id='name'
-              name='name'
-              value={formData.name}
-              onChange={handleChange}
-              placeholder="Meu evento"
-              required
-              className={errors.name ? 'input-error' : ''}
-            />
+      <div className="container-father">
+        <form noValidate onSubmit={handleSubmit}>
+          <label htmlFor="name">Nome do evento:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Meu evento"
+            required
+            className={errors.name ? "input-error" : ""}
+          />
 
-            <label htmlFor="photographer">Nome do fotógrafo:</label>
-            <input
-              type="text"
-              id='photographer'
-              name='photographer'
-              value={formData.photographer}
-              onChange={handleChange}
-              placeholder="Fotógrafo X"
-              required
-              className={errors.photographer ? 'input-error' : ''}
-            />
+          <label htmlFor="photographer">Nome do fotógrafo:</label>
+          <input
+            type="text"
+            id="photographer"
+            name="photographer"
+            value={formData.photographer}
+            onChange={handleChange}
+            placeholder="Fotógrafo X"
+            required
+            className={errors.photographer ? "input-error" : ""}
+          />
 
-            <label htmlFor="photographerLink">Link do fotógrafo:</label>
-            <input
-              type="url"
-              id='photographerLink'
-              name='photographerLink'
-              value={formData.photographerLink}
-              onChange={handleChange}
-              placeholder="www.fotografoX.com"
-              required
-              className={errors.photographerLink ? 'input-error' : ''}
-            />
+          <label htmlFor="photographerLink">Link do fotógrafo:</label>
+          <input
+            type="url"
+            id="photographerLink"
+            name="photographerLink"
+            value={formData.photographerLink}
+            onChange={handleChange}
+            placeholder="www.fotografoX.com"
+            required
+            className={errors.photographerLink ? "input-error" : ""}
+          />
 
-            <div className="album-upload-container">
-                <button className='albumUpload' onClick={() => document.getElementById('file-input')?.click()}>   
-                    <img src={Upload} alt="Upload icon" /> <span className='small-font'>Seu álbum de fotos</span>
-                </button>
-                <div className="album-text">Mande as fotos do seu álbum em um arquivo .zip (compactado)</div>
+          <div className="album-upload-container">
+            <button
+              className="albumUpload"
+              onClick={(event) => {
+                event.preventDefault(); // Prevent form submission
+                document.getElementById("file-input")?.click();
+              }}
+            >
+              <img src={Upload} alt="Upload icon" />
+              <span className="small-font">Seu álbum de fotos</span>
+            </button>
+            <div className="album-text">
+              Mande as fotos do seu álbum em um arquivo .zip (compactado)
             </div>
+          </div>
 
-            <input 
-              type="file" 
-              id="file-input" 
-              style={{ display: 'none' }} 
-              onChange={handleFileChange} 
-            />
+          <input
+            type="file"
+            id="file-input"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
 
-            <button className='button-confirm mt-8 mb-8' type="submit">+ Criar Evento</button>
-          </form>
-        </div> 
+          <button className="button-confirm mt-8 mb-8" type="submit">
+            + Criar Evento
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
